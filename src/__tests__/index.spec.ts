31748d0d805da886132b9ff7e2bce45d9ee17c46
Rowell Heria <rowell+dev@heria.uk>
2024-05-04 13:11:09 +0100

    style: apply prettier styles

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -4,23 +4,25 @@ import plugin from '../index';
 
 describe('vite-plugin-typed-i18next-loader', () => {
   it('should generate type definitions for default locale', async () => {
-    const result = await build({
+    const result = (await build({
       build: {
         lib: {
           entry: './src/__tests__/__fixtures__/entrypoint.ts',
-          formats: ['es']
+          formats: ['es'],
         },
-        outDir: './src/__tests__/__fixtures__/dist'
+        outDir: './src/__tests__/__fixtures__/dist',
       },
-      plugins: [plugin({
-        namespaceResolution: 'basename',
-        defaultLocale: 'en-GB',
-        output: './src/__tests__/__fixtures__/types/i18next.d.ts',
-        paths: ['./src/__tests__/__fixtures__/locales/'],
-      })],
-    }) as any[];
+      plugins: [
+        plugin({
+          namespaceResolution: 'basename',
+          defaultLocale: 'en-GB',
+          output: './src/__tests__/__fixtures__/types/i18next.d.ts',
+          paths: ['./src/__tests__/__fixtures__/locales/'],
+        }),
+      ],
+    })) as any[];
 
-    const output = result[0].output[0]
+    const output = result[0].output[0];
 
     expect(output.code).toMatchSnapshot();
   });
